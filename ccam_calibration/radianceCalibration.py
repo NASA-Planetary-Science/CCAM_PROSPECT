424be1d4ee732f586a0bffc42fbfa642c8a6e024
Colleen <colleen.oshea@jhuapl.edu>
2020-02-24 08:35:12 -0500

    add command line ovewrite option

--- ccam_calibration/radianceCalibration.py
+++ ccam_calibration/radianceCalibration.py
@@ -352,6 +352,8 @@ if __name__ == "__main__":
     parser.add_argument('-d', action="store", dest='directory', help="Directory containing .tab files")
     parser.add_argument('-l', action="store", dest='list', help="File with a list of .tab files")
     parser.add_argument('-o', action="store", dest='out_dir', help="directory to store the output files")
+    parser.add_argument('--no-overwrite', action="store_false", dest='overwrite', help="do not overwrite existing files")
+    parser.set_defaults(overwrite=True)
 
     args = parser.parse_args()
     if len(sys.argv) == 1:
@@ -372,4 +374,4 @@ if __name__ == "__main__":
     open(logfile, 'a').close()  # open file so it exists
 
     radianceCal = RadianceCalibration(logfile)
-    radianceCal.calibrate_to_radiance(in_file_type, in_file, args.out_dir)
+    radianceCal.calibrate_to_radiance(in_file_type, in_file, args.out_dir, args.overwrite)
