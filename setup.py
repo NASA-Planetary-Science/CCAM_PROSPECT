6a082c811f2e7847ce5c3cece1afa55a2f7b0ab6
Colleen <colleen.oshea@jhuapl.edu>
2021-02-25 16:42:26 -0500

    version number bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst", 'r') as f:
 
 setup(
    name='ccam_prospect',
-   version='1.3.1',
+   version='1.3.2',
    description='calibrate raw ccam files to rad and ref files',
    long_description=long_description,
    author='Colleen O\'Shea',
