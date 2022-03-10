4c97462ffb07ce56a7dcde200d767172cbd317b0
Colleen O'Shea <colleen.oshea@jhuapl.edu>
2022-03-10 14:44:07 -0500

    add utf-8 encoding to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
 from setuptools import setup, find_packages
 
 
 with open("README.md", 'r') as f:
     long_description = f.read()
-
 
 setup(
    name='ccam_prospect',
