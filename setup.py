7bb5d81dd230cfb3b8f858eb2d842c508f510dc7
Colleen O'Shea <colleen.oshea@jhuapl.edu>
2024-08-12 12:34:55 -0400

    update plots and mpl version

--- setup.py
+++ setup.py
@@ -16,5 +16,5 @@ setup(
    packages=['ccam_prospect', 'ccam_prospect.utils', 'ccam_prospect.constants', 'ccam_prospect.sol76', 'ccam_prospect.templates'],
    package_data={'ccam_prospect': ['constants/*', 'sol76/*', 'templates/*']
    },
-   install_requires=['numpy==1.19', 'jinja2', 'matplotlib==3.3.3', 'pds4_tools'],
+   install_requires=['numpy==1.19', 'jinja2', 'matplotlib', 'pds4_tools'],
 )
